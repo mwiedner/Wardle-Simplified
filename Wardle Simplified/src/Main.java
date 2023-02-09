72776d36b01308709dc170c7f0392aef31772b77
mwiedner <wiednermb@gmail.com>
2023-02-09 16:39:07 -0500

    Added a comment

--- Wardle Simplified/src/Main.java
+++ Wardle Simplified/src/Main.java
@@ -13,6 +13,7 @@ import java.time.format.FormatStyle;
 
 public class Main {
 
+// Strings used to format the colors in the console
 public static final String ANSI_RESET = "\u001B[0m";
 public static final String ANSI_GREEN = "\u001B[32m";
 public static final String ANSI_YELLOW = "\033[0;93m";
